b3fbc8ff72e66c0d46c5dcd85194184064f46dce
Ross Delinger <rossdylan@csh.rit.edu>
2012-07-18 12:22:03 -0400

    Added Metrology metrics to all the views

--- tahrir/views.py
+++ tahrir/views.py
@@ -1,4 +1,4 @@
-
+import Metrology
 import transaction
 import types
 
@@ -21,64 +21,83 @@ from pyramid.security import (
 import tahrir_api.model as m
 import widgets
 
+#  Metrics definitions
+reporter = Metrology.GraphiteReporter('graphite.local', 2003)
+reporter.start()
+page_view_meter = Metrology.meter('page-views')
+admin_view_timer = Metrology.timer('admin-view-timer')
+login_view_timer = Metrology.timer('login-view-timer')
+logout_view_timer = Metrology.timer('logout-view-timer')
+openid_cb_timer = Metrology.timer('openid-cb-timer')
+index_view_timer = Metrology.timer('index-view-timer')
+
 
 # TODO -- really wield tw2.sqla here
 @view_config(route_name='admin', renderer='admin.mak')
 def admin(request):
-    logged_in = authenticated_userid(request)
+    #  Metrics stuff
+    page_view_meter.mark()
+    page_view_meter.count
+    with admin_view_timer:
+        logged_in = authenticated_userid(request)
 
-    if logged_in != request.registry.settings['tahrir.admin']:
-        return HTTPFound(location='/')
+        if logged_in != request.registry.settings['tahrir.admin']:
+            return HTTPFound(location='/')
 
-    is_awarded = lambda a: logged_in and a.person.email == logged_in
-    awarded_assertions = filter(is_awarded, m.Assertion.query.all())
+        is_awarded = lambda a: logged_in and a.person.email == logged_in
+        awarded_assertions = filter(is_awarded, m.Assertion.query.all())
 
-    name_lookup = {
-        'issuerform': widgets.IssuerForm,
-        'badgeform': widgets.BadgeForm,
-        'assertionform': widgets.AssertionForm,
-        'personform': widgets.PersonForm,
-    }
+        name_lookup = {
+            'issuerform': widgets.IssuerForm,
+            'badgeform': widgets.BadgeForm,
+            'assertionform': widgets.AssertionForm,
+            'personform': widgets.PersonForm,
+        }
 
-    for key in name_lookup:
-        if any([k.startswith(key) for k in request.params]):
-            w = name_lookup[key]
+        for key in name_lookup:
+            if any([k.startswith(key) for k in request.params]):
+                w = name_lookup[key]
 
-            try:
-                data = w.validate(request.params)
-                w.validated_request(request, data,
-                                    protect_prm_tamp=False)
-                return HTTPFound(location='/admin')
-            except twc.ValidationError as e:
-                print e.widget
+                try:
+                    data = w.validate(request.params)
+                    w.validated_request(request, data,
+                                        protect_prm_tamp=False)
+                    return HTTPFound(location='/admin')
+                except twc.ValidationError as e:
+                    print e.widget
 
-    return dict(
-        is_admin=True,
-        logged_in=logged_in,
-        awarded_assertions=awarded_assertions,
-        base_url=request.registry.settings['tahrir.base_url'],
-        title=request.registry.settings['tahrir.title'],
-        issuer_form=widgets.IssuerForm,
-        badge_form=widgets.BadgeForm,
-        assertion_form=widgets.AssertionForm,
-        person_form=widgets.PersonForm,
-    )
+        return dict(
+            is_admin=True,
+            logged_in=logged_in,
+            awarded_assertions=awarded_assertions,
+            base_url=request.registry.settings['tahrir.base_url'],
+            title=request.registry.settings['tahrir.title'],
+            issuer_form=widgets.IssuerForm,
+            badge_form=widgets.BadgeForm,
+            assertion_form=widgets.AssertionForm,
+            person_form=widgets.PersonForm,
+        )
 
 
 @view_config(route_name='home', renderer='index.mak')
 def index(request):
-    logged_in = authenticated_userid(request)
-    is_admin = logged_in == request.registry.settings['tahrir.admin']
-    is_awarded = lambda a: logged_in and a.person.email == logged_in
-    awarded_assertions = filter(is_awarded, m.Assertion.query.all())
-    return dict(
-        is_admin=is_admin,
-        issuers=m.Issuer.query.all(),
-        awarded_assertions=awarded_assertions,
-        base_url=request.registry.settings['tahrir.base_url'],
-        logged_in=logged_in,
-        title=request.registry.settings['tahrir.title'],
-    )
+     #  Metrics stuff
+    page_view_meter.mark()
+    page_view_meter.count
+
+    with index_view_timer:
+        logged_in = authenticated_userid(request)
+        is_admin = logged_in == request.registry.settings['tahrir.admin']
+        is_awarded = lambda a: logged_in and a.person.email == logged_in
+        awarded_assertions = filter(is_awarded, m.Assertion.query.all())
+        return dict(
+            is_admin=is_admin,
+            issuers=m.Issuer.query.all(),
+            awarded_assertions=awarded_assertions,
+            base_url=request.registry.settings['tahrir.base_url'],
+            logged_in=logged_in,
+            title=request.registry.settings['tahrir.title'],
+        )
 
 
 @view_config(context=types.FunctionType)
@@ -107,41 +126,54 @@ def json(context, request):
 @view_config(route_name='login', renderer='login.mak')
 @forbidden_view_config(renderer='login.mak')
 def login(request):
-    login_url = request.resource_url(request.context, 'login')
-    referrer = request.url
-    if referrer == login_url:
-        referrer = '/'  # never use the login form itself as came_from
-    came_from = request.params.get('came_from', referrer)
-    request.session['came_from'] = came_from
-    return dict(
-        openid_url=request.registry.settings['openid.provider'],
-        title=request.registry.settings['tahrir.title'],
-        url="http://" + request.registry.settings['tahrir.base_url'] + '/dologin.html',
-        came_from=came_from,
-        )
+    #  Metrics stuff
+    page_view_meter.mark()
+    page_view_meter.count
+    with login_view_timer:
+        login_url = request.resource_url(request.context, 'login')
+        referrer = request.url
+        if referrer == login_url:
+            referrer = '/'  # never use the login form itself as came_from
+        came_from = request.params.get('came_from', referrer)
+        request.session['came_from'] = came_from
+        return dict(
+            openid_url=request.registry.settings['openid.provider'],
+            title=request.registry.settings['tahrir.title'],
+            url="http://" + request.registry.settings['tahrir.base_url'] + '/dologin.html',
+            came_from=came_from,
+            )
 
 
 @view_config(route_name='logout')
 def logout(request):
-    headers = forget(request)
-    return HTTPFound(location=request.resource_url(request.context),
-                     headers=headers)
+    #  Metrics stuff
+    page_view_meter.mark()
+    page_view_meter.count
+    with logout_view_timer:
+        headers = forget(request)
+        return HTTPFound(location=request.resource_url(request.context),
+                         headers=headers)
 
 def openid_success(context, request, *args, **kwargs):
-    identity = request.params['openid.identity']
-    email = request.params['openid.sreg.email']
-    if not identity.startswith(request.registry.settings['openid.provider']):
-        request.session.flash(
-        'Invalid OpenID provider. You can only use{0}'.format(
-            request.registry.settings['openid.provider']))
-        return HTTPFound(location=request.application_url + '/login')
-    username = identity.split("/")[-1]
-    if m.Person.query.filter_by(email=email).count() == 0:
-        new_user = m.Person(email=email)
-        m.DBSession.add(new_user)
-    headers = remember(request, email)
-    came_from = request.session['came_from']
-    del(request.session['came_from'])
-    response = HTTPFound(location=came_from)
-    response.headerlist.extend(headers)
-    return response
+    #  Metrics stuff
+    page_view_meter.mark()
+    page_view_meter.count
+
+    with openid_cb_timer:
+        identity = request.params['openid.identity']
+        email = request.params['openid.sreg.email']
+        if not identity.startswith(request.registry.settings['openid.provider']):
+            request.session.flash(
+            'Invalid OpenID provider. You can only use{0}'.format(
+                request.registry.settings['openid.provider']))
+            return HTTPFound(location=request.application_url + '/login')
+        username = identity.split("/")[-1]
+        if m.Person.query.filter_by(email=email).count() == 0:
+            new_user = m.Person(email=email)
+            m.DBSession.add(new_user)
+        headers = remember(request, email)
+        came_from = request.session['came_from']
+        del(request.session['came_from'])
+        response = HTTPFound(location=came_from)
+        response.headerlist.extend(headers)
+        return response
